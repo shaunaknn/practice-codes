2edd10cbd383256b49dd5d1999edd31aed470f27
shaunaknn <shaunaknn@gmail.com>
2026-04-09 11:51:21 +0200

    added ISA atmosphere model (not implemented yet)

--- RDI_Project/Rocket_sim.py
+++ RDI_Project/Rocket_sim.py
@@ -2,24 +2,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-# Constants
-
+# Gravity model
 g0 = 9.81              # Sea-level gravity (m/s^2)
 R_earth = 6371000      # Earth radius (m)
-rho0 = 1.225           # Sea-level density (kg/m^3)
+
+def gravity(h): # Newton's law of gravity
+    return g0 * (R_earth / (R_earth + h))**2
+
+# c = np.sqrt(gamma*R_air*T)
+
+# Basic atmosphere model
+#def air_density(h):
+#    return rho0 * np.exp(-h / H)
+
+### ISA Atmosphere model
+
+# General Constants
+gamma = 1.4            # Adiabatic constant for air
+R_air = 287.05         # Gas constant (J/kgK)
+
+# Troposhphere
+L_tr = -0.0065         # Temperature lapse rate in troposphere (K/m)
+T0 = 288.15            # Sea-level Temperature
+rho0 = 1.225           # Sea-level density (kg/m^3) 
+p0 = 101325            # Sea-level pressure (Pa)
+
+# Lower Stratorsphere - 11 - 20 km
+T11 = 216.65           # Temperature at 11 km (constant)
+p11 = 22632            # Pressure at 11 km
+rho11 = 0.364          # Density at 11 km (kg/m^3)
+
+# Upper Stratosphere - 20 - 32 km
+T20 = 216.65           # Temperature at 20 km
+L_ustr = 0.001         # Temperature lapse rate in upper stratosphere
+p20 = 5474.88          # Pressure at 20 km
+rho20 = 0.088          # Density at 20 km (kg/m^3)
+
+def isa_atmosphere(h):
+    '''
+    returns T(h), rho(h) and p(h) at given height
+    '''
+    if h < 11000:  # Troposphere
+        T = T0 + L_tr * h
+        p = p0 * (T / T0)**(-g0 / (L_tr * R_air)) # polytropic relation
+    elif h >= 11000 and h < 20000:  # Lower Stratosphere
+        T = T11
+        p = p11 * np.exp(-g0 * (h - 11000) / (R_air * T))
+    elif h >= 20000 and h < 32000:
+        T = T20 + L_ustr*(h - 20000)
+        p = p20 * (T / T20)**(-g0/ (L_ustr* R_air))
+    else:
+        raise ValueError("Altitude exceeds ISA model range (>32 km)")
+    rho = p / (R_air * T)
+    return T, p, rho
+
 H = 8500               # Scale height (m)
 
+# Aerodynamic parameters
 Cd = 0.5               # Drag coefficient
 A = 0.1                # Cross-sectional area (m^2)
 
 # Rocket parameters
-T = 15000              # Thrust (N)
+Th = 15000              # Thrust (N)
 Isp = 300              # Specific impulse (s)
 
 m0 = 500               # Initial mass (kg)
 mf = 200               # Final mass (kg)
 
-mdot = T / (Isp * g0)  # Mass flow rate (kg/s)
+mdot = Th / (Isp * g0)  # Mass flow rate (kg/s)
 burn_time = (m0 - mf) / mdot
 
 # Pitch Program
@@ -37,18 +87,6 @@ def pitch_program(t):
     else:
         return np.deg2rad(50)  # near horizontal
 
-
-
-# Atmosphere Model
-
-def air_density(h):
-    return rho0 * np.exp(-h / H)
-
-# Gravity Model
-
-def gravity(h):
-    return g0 * (R_earth / (R_earth + h))**2
-
 # ODE System
 
 def rocket_ode(t, y):
@@ -58,14 +96,15 @@ def rocket_ode(t, y):
     h = max(z, 0)
 
     # Forces
-    rho = air_density(h)
+    #rho = air_density(h)
+    T, p, rho = isa_atmosphere(h)
     D = 0.5 * rho * Cd * A * v**2
 
     g = gravity(h)
 
     # Thrust only during burn
     if t <= burn_time and m > mf:
-        thrust = T
+        thrust = Th
         dm_dt = -mdot
     else:
         thrust = 0
